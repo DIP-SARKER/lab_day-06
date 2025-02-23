291d3b797b8062b6318c87faecb4790a65124c9a
DIP-SARKER <sarker15-5274@diu.edu.bd>
2025-02-23 12:58:56 +0600

    finalcommit

--- lib/appbody.dart
+++ lib/appbody.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:icons_plus/icons_plus.dart';
+// import 'package:icons_plus/icons_plus.dart';
 
 class Appbody extends StatefulWidget {
   const Appbody({super.key});
@@ -9,14 +9,32 @@ class Appbody extends StatefulWidget {
 }
 
 class _AppbodyState extends State<Appbody> {
+  int currentIndex = 0;
+  final List<Widget> pages = [
+    Text("Home"),
+    Text("Exit"),
+  ];
+
+  void onTapMethod(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(
-            icon: Icon(MingCute.currency_bitcoin_fill), label: "Crypto"),
-      ]),
+      body: Center(child: pages[currentIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: onTapMethod,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.exit_to_app),
+              label: "Exit",
+            ),
+          ]),
     );
   }
 }
