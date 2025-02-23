697208bd61bc7be19c0abc29ba4fe6ff7c71b104
DIP-SARKER <sarker15-5274@diu.edu.bd>
2025-02-23 12:42:59 +0600

    bottomNavBar

---
+++ lib/appbody.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+
+class Appbody extends StatefulWidget {
+  const Appbody({super.key});
+
+  @override
+  State<Appbody> createState() => _AppbodyState();
+}
+
+class _AppbodyState extends State<Appbody> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(
+            icon: Icon(MingCute.currency_bitcoin_fill), label: "Crypto"),
+      ]),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:cached_network_image/cached_network_image.dart';
+//import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/appbody.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         drawer: Drawer(
           child: ListView(
             children: [
-              Container(
+              SizedBox(
                 height: 70,
                 child: DrawerHeader(
                   child: Row(
@@ -41,41 +42,52 @@ class MyApp extends StatelessWidget {
               ListTile(
                 leading: Icon(Icons.people),
                 title: Text("User"),
+                trailing: Icon(Icons.arrow_forward),
                 onTap: () {},
               ),
               ListTile(
                 leading: Icon(Icons.account_box),
                 title: Text("Contact"),
+                trailing: Icon(Icons.arrow_forward),
                 onTap: () {},
               ),
               ListTile(
                 leading: Icon(Icons.edit),
                 title: Text("Edit"),
+                trailing: Icon(Icons.arrow_forward),
                 onTap: () {},
               ),
               ListTile(
                 leading: Icon(Icons.balance),
                 title: Text("Balance"),
+                trailing: Icon(Icons.arrow_forward),
                 onTap: () {},
               ),
               ListTile(
                 leading: Icon(Icons.wifi),
                 title: Text("WiFi"),
+                trailing: Icon(Icons.arrow_forward),
                 onTap: () {},
               ),
               ListTile(
                 leading: Icon(Icons.share),
                 title: Text("Share"),
+                trailing: Icon(Icons.arrow_forward),
                 onTap: () {},
               ),
               ListTile(
                 leading: Icon(Icons.dangerous_outlined),
                 title: Text("Cross"),
+                trailing: Icon(Icons.arrow_forward),
                 onTap: () {},
               ),
               ListTile(
                 leading: Icon(Icons.alarm),
                 title: Text("Clock"),
+                trailing: TextButton(
+                  onPressed: () {},
+                  child: Text("GO"),
+                ),
                 onTap: () {},
               ),
             ],
@@ -86,18 +98,7 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.blue,
           centerTitle: true,
         ),
-        body: Column(
-          children: [
-            Text("Hello World"),
-            CachedNetworkImage(
-              imageUrl:
-                  "https://cdn.pixabay.com/photo/2023/09/06/02/21/ice-8236115_960_720.jpg",
-              placeholder: (context, url) =>
-                  Center(child: CircularProgressIndicator()),
-              errorWidget: (context, url, error) => Icon(Icons.error),
-            ),
-          ],
-        ),
+        body: Appbody(),
       ),
     );
   }
